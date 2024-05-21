5a35e0ec30e84f9647917ab00f31483736ddd1e8
Tadeo Wotoszyn <tadeowoto@MacBook-Pro-de-Tadeo.local>
2024-05-21 15:16:09 -0300

    funcionalidad del boton agregada

---
+++ script/script.js
@@ -0,0 +1,16 @@
+
+
+artistBtn = document.getElementById('next-artist-btn');
+
+
+artistBtn.addEventListener('click', function() {
+    let sectionArtistas = document.querySelectorAll('.main-wrapper');
+    let posActual = window.scrollY;
+    for (let i = 0; i < sectionArtistas.length; i++) {
+        let sectionTop = sectionArtistas[i].offsetTop;
+        if (sectionTop > posActual + 10) {
+            window.scrollTo({ top: sectionTop, behavior: 'smooth' });
+            break;
+        }
+    }
+});
